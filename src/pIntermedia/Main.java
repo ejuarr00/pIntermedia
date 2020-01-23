e08ce697fec809c2e1faf8ef4fc7be3204f521af
Eduardo Juárez Robles <ejuarr00@estudiantes.unileon.es>
2020-01-23 20:36:48 +0100

    con alguna posible modificación del examen

--- src/pIntermedia/Main.java
+++ src/pIntermedia/Main.java
@@ -3,8 +3,11 @@ package pIntermedia;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class Main {
 	private static int numeroHojas;
+	private static int parametroen2linea=0;
+	private static int parametroLineanueva=0;
 	public static void main(String[] args){
 		ArrayList<String[][]> listaHojas=lectura();
 		for(int i=0; i<listaHojas.size();i++){
@@ -29,23 +32,72 @@ public class Main {
 	public static void imprimeHojaSalida(int[][] hojaSalida) {
 		for(int f=0;f<hojaSalida.length;f++){
 			for(int c=0; c<hojaSalida[f].length;c++){
+				
 				if(c==hojaSalida[f].length-1){// si estoy ultima columna
-					System.out.print(hojaSalida[f][c]);
+					if(hojaSalida[f][c]==5555) {
+						System.out.print((HojaExcel.getHojaEntrada()[f][c]));
+					}else {
+						System.out.print(hojaSalida[f][c]);
+					}
+					
 				}else{
-					System.out.print(hojaSalida[f][c]+" ");
+					if(hojaSalida[f][c]==5555) {
+						System.out.print((HojaExcel.getHojaEntrada()[f][c]+" "));
+					}else {
+						System.out.print(hojaSalida[f][c]+" ");
+					}	
 				}
 			}
 			System.out.println();
 		}
 	}
+	
+	//***************************************imprimir suma datos de filas totales o numero mayor
+		public static void imprimeHojaSalida1(String[][] hojaEntrada) {
+			//int sumaTotal=0;
+			//int sumafilaspares=0;
+			//int mayor=hojaSalida[0][0];
+			//int multiplos6=0;
+			String aux = "";
+			for(int f=0;f<hojaEntrada.length;f++){
+				for(int c=0; c<hojaEntrada[f].length;c++){
+					if(!HojaExcel.esNumero(hojaEntrada[f][c])&&!HojaExcel.esFormula(hojaEntrada[f][c])) {
+						aux=aux+hojaEntrada[f][c];
+						
+					}
+					/*sumaTotal+=hojaSalida[f][c];
+					if(hojaSalida[f][c]>mayor) {
+						mayor=hojaSalida[f][c];
+					}
+					if(f%2==0) {
+						sumafilaspares+=hojaSalida[f][c];
+					}
+					System.out.println(parametroen2linea+ "lineeeeeeeeee");
+					if(hojaSalida[f][c]%parametroen2linea==0) {
+						multiplos6+=1;
+					}*/
+				}
+			}
+			System.out.println(aux);
+			System.out.println();
+			/*System.out.println("la suma filas pares"+sumafilaspares);
+			System.out.println("la suma total es"+ sumaTotal);
+			System.out.println("el mayor es "+ mayor);
+			System.out.println("multiplos de 6 "+ multiplos6);*/
+		}
 
 	//metodo para leer todo 
 	private static ArrayList<String[][]>  lectura() {
 		Scanner sc=new Scanner(System.in);
 		//numero de hojas
-		numeroHojas=Integer.parseInt(sc.nextLine());
+		String numHojas=sc.nextLine();
+		if(HojaExcel.esNumero(numHojas)==false){
+			System.out.println("Entrada Inválida. Número de hojas debe ser un número.");
+			System.exit(0);
+		}
+		numeroHojas=Integer.parseInt(numHojas);
 		if(numeroHojas<1){
-			System.out.println("Entrada Inválida. Número de hojas debe ser mayor que 0");
+			System.out.println("Entrada Inválida. Número de hojas debe ser mayor que 0.");
 			System.exit(0);
 		}
 		ArrayList<String[][]> listaHojas= new ArrayList<String[][]>();
@@ -54,17 +106,29 @@ public class Main {
 			//primera linea cada hoja: leo columna y filas
 			String linea=sc.nextLine();
 			String []lineaColumnasyFilas= linea.split(" ");
+			if(HojaExcel.esNumero(lineaColumnasyFilas[0])==false){
+				System.out.println("Entrada Inválida. Número de columnas debe ser un número.");
+				System.exit(0);
+			}
 			int columnas=Integer.parseInt(lineaColumnasyFilas[0]);
 			if(columnas<1||columnas>=18278){
-				System.out.println("Entrada Inválida. El Número de columnas debe estar entre 1 y 128278");
+				System.out.println("Entrada Inválida. El Número de columnas debe estar entre 1 y 128278.");
 				System.exit(0);
 			}
 			//System.out.println(columnas+" colummnasas");
-			int filas=Integer.parseInt(lineaColumnasyFilas[1]);
-			if(filas<1||filas>=999){
-				System.out.println("Entrada Inválida. Número de filas debe estar entre 1 y 999");
+			if(HojaExcel.esNumero(lineaColumnasyFilas[1])==false){
+				System.out.println("Entrada Inválida. Número de filas debe ser un número.");
 				System.exit(0);
 			}
+			int filas=Integer.parseInt(lineaColumnasyFilas[1]);
+			if(filas<1||filas>=999){
+				System.out.println("Entrada Inválida. Número de filas debe estar entre 1 y 999.");
+				System.exit(0);
+			}
+			
+			//parametroen2linea=Integer.parseInt(lineaColumnasyFilas[2]);
+			
+			//parametroLineanueva=Integer.parseInt(sc.nextLine());
 			//System.out.println(filas+" filas");
 			//creo la hoja de excel con el numero de filas y columnas
 			String[][] tablaExcel=new String[filas][columnas];
@@ -78,7 +142,7 @@ public class Main {
 					tablaExcel[f][c]=lineaColVector[c];
 					if(lineaColVector.length!=columnas){
 						//System.out.println(lineaColVector.length+" tamaño leido columnas");
-						System.out.println("Entrada Inválida. Número de columnas distinto de las especificadas");
+						System.out.println("Entrada Inválida. Número de columnas distinto de las especificadas.");
 						System.exit(0);
 					}
 					//System.out.println(lineaColVector[c]+"content");
